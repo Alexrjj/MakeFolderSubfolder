d3a2c3d76b1c07c15d019adf85350a4bb06fbfd2
Alex Piter <alexsoares2517@hotmail.com>
2018-02-20 15:20:26 -0300

    adiciona aviso alteração data, copia fluxo padrão da pasta raíz

--- makeFolderSubfolder.py
+++ makeFolderSubfolder.py
@@ -8,12 +8,17 @@ wb = openpyxl.load_workbook('pastas.xlsx')
 for sheet in wb.worksheets:
     path = sheet['A2'].value
     for row in sheet.iter_cols(min_row=2, min_col=2, max_col=2):
+        input('Change the data in xlsm file and press any key to continue...')
         for cell in row:
             os.makedirs(os.path.join(path, str(cell.value)))
-            shutil.copy2('C:/Users/Admin/Google Drive/Arquivos/FluxoObraPadrao4.0.xlsm', os.path.join(path, str(cell.value)))
+            shutil.copy2('FluxoObraPadrao4.0.xlsm', os.path.join(path, str(cell.value)))
             os.rename(os.path.join(path, str(cell.value), 'FluxoObraPadrao4.0.xlsm'), (os.path.join(path, str(cell.value), str(cell.value) + '.xlsm')))
-            # wb1 = openpyxl.load_workbook(os.path.join(path, str(cell.value), str(cell.value) + '.xlsm'), read_only=False, keep_vba=True)
-            # sheet2 = wb1['Ficha_Solicitação_Material']
-            # sheet2['H5'] = str(cell.value)
-            # wb1.save(str(cell.value + '.xlsm'))
+            '''
+            Código abaixo não funcional, pois ao inserir o número da SOB na célula específica, e salvar o arquivo,
+            perde-se boa parte da formatação do mesmo.
+            wb1 = openpyxl.load_workbook(os.path.join(path, str(cell.value), str(cell.value) + '.xlsm'), read_only=False, keep_vba=True)
+            sheet2 = wb1['Ficha_Solicitação_Material']
+            sheet2['H5'] = str(cell.value)
+            wb1.save(str(cell.value + '.xlsm'))
+            '''
             time.sleep(3)
