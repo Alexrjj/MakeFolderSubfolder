db44dacbfead53fb410399eaaa9bfb4e159a2aa8
Alex Piter <alexsoares2517@hotmail.com>
2018-02-21 11:38:20 -0300

    cria pasta apenas após inserir data no arquivo xlsm

--- makeFolderSubfolder.py
+++ makeFolderSubfolder.py
@@ -8,7 +8,16 @@ wb = openpyxl.load_workbook('pastas.xlsx')
 for sheet in wb.worksheets:
     path = sheet['A2'].value
     for row in sheet.iter_cols(min_row=2, min_col=2, max_col=2):
-        input('Change the date in xlsm file to ' + sheet['A2'].value + ' and press enter key to continue...')
+        os.startfile('FluxoObraPadrao4.0.xlsm')
+        for x in range(0, 1000):
+            try:
+                xls = open('FluxoObraPadrao4.0.xlsm', 'r+')
+                if xls:
+                    xls.close()
+                    break
+            except IOError:
+                time.sleep(1)
+                continue
         for cell in row:
             os.makedirs(os.path.join(path, str(cell.value)))
             shutil.copy2('FluxoObraPadrao4.0.xlsm', os.path.join(path, str(cell.value)))
